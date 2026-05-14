336fab713299280a6b84f0abadca73d3a7cda434
Irgi Harnoyo <rasqasakim@gmail.com>
2026-05-14 12:34:32 +0700

    ready for render deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = 3000
+const PORT = Number(process.env.PORT) ||  3000
 
 app.listen(PORT, () => {
     console.log(`server running on https://localhost:${PORT}`);
